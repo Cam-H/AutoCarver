b97497b0f1b2ecf41e271a7a0d9dfac1da9af7e0
Cameron H <ca.hatherell@gmail.com>
2024-04-23 18:11:06 -0400

    Minimal application setup

---
+++ src/Core/EntryPoint.cpp
@@ -0,0 +1,14 @@
+#include <QApplication>
+#include <QPushButton>
+
+#include <QLabel>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    QPushButton button("Hello world!", nullptr);
+    button.resize(200, 100);
+    button.show();
+
+    return QApplication::exec();
+}
